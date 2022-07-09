425fbf4793291a2183ae5f8926a1b9a22f838dd1
Aman Malik <amanmalik@192.168.1.4>
2022-07-09 12:45:32 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const path = require("path")
 const app = express();
+const port = process.env.PORT || 3000
 
 const pathToDir = path.join(__dirname, '../public')
 
 app.use(express.static(pathToDir))
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('starting server');
 })
